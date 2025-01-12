02ca417b5a2666a6557532cf00c87539fe92c91c
chilltomato <haz23003@byui.edu>
2025-01-11 19:04:12 -0500

    first two things done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your first name? ");
+        string gradestring = Console.ReadLine();
+        int grade = int.Parse(gradestring);
+
+        string letter = "z";
+
+        if (grade >= 90){
+            letter = "A";
+        }
+        else if (grade >= 80){
+            letter = "B";
+        }
+        else if (grade >= 70){
+            letter = "C";
+        }
+        else if (grade >= 60){
+            letter = "D";
+        }
+        else if (grade < 60){
+            letter = "F";
+        }
+
+        Console.WriteLine($"your grade is {gradestring} {letter}.");
+
+        if (grade >= 70) {
+        Console.WriteLine($"you passed the class");
+        }
+        else{
+        Console.WriteLine($"you failed the class");
+        }
+
     }
 }
